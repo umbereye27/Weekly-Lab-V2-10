2ff79973c29a6f46bb6595fab90cd7f1a04d2fcb
umbereye27 <carineumbereye7@gmail.com>
2025-07-10 20:39:01 +0200

    refactor(build-errors): removed middleware authentication for a while

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,6 @@
-import authConfig from "./auth.config"
-import NextAuth from "next-auth"
- import { NextRequest } from "next/server"
-// Use only one of the two middleware options below
-// 1. Use middleware directly
-// export const { auth: middleware } = NextAuth(authConfig)
- 
-// 2. Wrapped middleware option
-const { auth } = NextAuth(authConfig)
-export default auth(async function middleware(req: NextRequest) {
-  // Your custom middleware logic goes here
-})
+import { NextRequest } from "next/server";
+
+// Example of default export
+export default function middleware(request: NextRequest) {
+  // Middleware logic
+}
